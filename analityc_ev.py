0075216f8643d451d0501ed812c3ed3e1b602a5b
Luis <miguel.galvis@udea.edu.co>
2023-11-02 15:06:42 -0500

    Formula

--- analityc_ev.py
+++ analityc_ev.py
@@ -3,3 +3,7 @@ import matplotlib.pyplot as plt
 from qutip import *
 from tqdm import tqdm
 import math as math
+
+#Veamos la inversión de población para un pulso actuando sobre un estado inicial venido del Jaynes-Cummings
+
+#Definamos las funciones que calculan
